7eeb05cb9db6ac296d6f388b5a4ac7d6c39512fd
ChristopherVH <cvanhoosier@gmail.com>
2016-09-03 15:44:25 -0700

    working semi-perfectly

--- background.js
+++ background.js
@@ -8,15 +8,17 @@ script.type = 'text/javascript';
 script.src = "https://apis.google.com/js/client.js?onload=callbackFunction";
 head.appendChild(script);
 var accessToken;
+var authInstance;
+
 
 var CLIENT_ID = '6344508761-1uii1p3je2jnt4innp07fbk8rvq66976.apps.googleusercontent.com';
-var SCOPES = ['https://www.googleapis.com/auth/drive'];
+var SCOPES = ['https://www.googleapis.com/auth/documents'];
 
 function checkAuth() {
  gapi.auth.authorize(
    {
      'client_id': CLIENT_ID,
-     'scope': SCOPES.join(' '),
+     'scope': SCOPES,
      'immediate': true
    }, makeSureAuthorized);
 }
@@ -26,6 +28,7 @@ function makeSureAuthorized(authResult) {
         {client_id: CLIENT_ID, scope: SCOPES, immediate: false});
       return false;
     }
+  accessToken = gapi.auth.getToken().access_token;
 }
 
 
@@ -75,24 +78,34 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
    case "Url":
    chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
      docUrl = tabs[0].url;
-    //  checkAuth();
-      chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
-        console.log(token);
-        accessToken = token;
-        gapi.auth.setToken({
-          'access_token': token
-        });
-      });
+    //  authInstance = gapi.auth2.getAuthInstance();
+    //  var options = new gapi.auth2.SigninOptionsBuilder();
+    //  options.setScope('drive');
+    //  authInstance.signIn(options);
+    //  authInstance.isSignedIn.listen(function(isCurrentlySingedIn){
+    //    if (isCurrentlySingedIn === false){
+    //      authInstance.signOut();
+    //    }
+    //  });
+    checkAuth();
+      //
+    //   chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
+    //     console.log(token);
+    //     accessToken = token;
+    //   });
     });
        break;
   }
 });
+
+
 
 function captureCallback(callback){
   chrome.tabs.query({active: true, currentWindow: true}, function(tabArray){
     chrome.tabs.executeScript(tabArray[0].id, {
       code : "window.getSelection().toString()"
     }, function(results){
+      console.log(results);
       callback(results[0]);
     });
   });
@@ -103,58 +116,64 @@ function captureCallback(callback){
 
 
 function putInDoc(dataobj){
-  if (running === false){
-    running = true;
-    $.ajax({
-       url: "https://script.google.com/macros/s/AKfycbwADtcsloxMU9h-_1Y_FXjW6JifgkWTeW-qh6fO0hd9ye8T5OTq/exec",
-       data: dataobj,
-       type: "GET",
-       success: function(){
-         running = false;
-         if (ajaxQueue.length > 0){
-           putInDoc(ajaxQueue.shift());
-         }
-       },
-       beforeSend : function( xhr ) {
-        xhr.setRequestHeader( 'Authorization', 'BEARER ' + accessToken );
-       }
-     });
-  }else{
-    ajaxQueue.push(dataobj);
-  }
+  // if (running === false){
+  //   running = true;
+  //   $.ajax({
+  //      url: "https://script.google.com/macros/s/AKfycbwADtcsloxMU9h-_1Y_FXjW6JifgkWTeW-qh6fO0hd9ye8T5OTq/exec",
+  //      data: dataobj,
+  //      type: "GET",
+  //      success: function(){
+  //        running = false;
+  //        if (ajaxQueue.length > 0){
+  //          putInDoc(ajaxQueue.shift());
+  //        }
+  //      },
+  //      beforeSend : function( xhr ) {
+  //       xhr.setRequestHeader( 'Authorization', 'Bearer ' + accessToken );
+  //      }
+  //    });
+  // }else{
+  //   ajaxQueue.push(dataobj);
+  // }
 
-   // could add to queue and call in order based of ajaxComplete.
-//   var scriptId = "1pXWyWWSV05dJyloHFHRORnn9yrZxXqYu6AI8Md9UjCC_PSRUAGK1Kh67";
-//
-//  // Create execution request.
-//    var request = {
-//        'function': 'insertText',
-//        'parameters': dataobj,
-//         "devMode": true
-//    };
-//
-//  // Make the request.
-//    var op = gapi.client.request({
-//         'root': 'https://script.googleapis.com',
-//         'path': 'v1/scripts/' + scriptId + ':run',
-//         'body': request
-//    });
-//
-//  op.execute(function(resp) {
-//   if (resp.error && resp.error.status) {
-//     // The API encountered a problem before the script started executing.
-//     console.log('Error calling API: ' + JSON.stringify(resp, null, 2));
-//   } else if (resp.error) {
-//     // The API executed, but the script returned an error.
-//     var error = resp.error.details[0];
-//     console.log('Script error! Message: ' + error.errorMessage);
-//   } else {
-//     // Here, the function returns an array of strings.
-//     console.log("success!");
-//   }
-// });
+  var scriptId = "1pXWyWWSV05dJyloHFHRORnn9yrZxXqYu6AI8Md9UjCC_PSRUAGK1Kh67";
+
+ // Create execution request.
+   var request = {
+      'function': 'doGet',
+       'parameters': [dataobj],
+        "devMode": true
+   };
+
+ // Make the request.
+   var op = gapi.client.request({
+        'root': 'https://script.googleapis.com',
+        'path': 'v1/scripts/' + scriptId + ':run',
+        'method': 'POST',
+        'body': request
+   });
+ //
+ op.execute(function(resp) {
+  if (resp.error && resp.error.status) {
+    // The API encountered a problem before the script started executing.
+    console.log('Error calling API: ' + JSON.stringify(resp, null, 2));
+  } else if (resp.error) {
+    // The API executed, but the script returned an error.
+    var error = resp.error.details[0];
+    console.log('Script error! Message: ' + error.errorMessage);
+  } else {
+    // Here, the function returns an array of strings.
+    console.log(resp);
+    console.log("success!");
+  }
+  });
 }
 
+chrome.identity.onSignInChanged.addListener(
+  function(){
+    chrome.identity.removeCachedAuthToken(accessToken);
+  }
+);
 
 chrome.commands.onCommand.addListener(function (command){
   var obj = {};
